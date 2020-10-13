8b0bae83ec483b988ebc7d413c39282d3a419f0e
Sharma <deepakkumar.a.sharma@accenture.com>
2020-10-13 18:14:20 +0530

    Production build

---
+++ build/precache-manifest.6f31f3c59160d6bec8c44198292e2b62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b796a5c7f48e87e8e2fc53a289199a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d7ac2e7c8b1f192f968",
+    "url": "/static/js/2.dc9a6f9d.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.dc9a6f9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c23ed343d9afaf9a2145",
+    "url": "/static/js/main.2023db29.chunk.js"
+  },
+  {
+    "revision": "67cdef6326c39928f3da",
+    "url": "/static/js/runtime-main.4226f3c8.js"
+  }
+]);
